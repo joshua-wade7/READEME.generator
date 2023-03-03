e8dd7373867848fb45111f7f2bd9da8d9d3017fc
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-03-02 21:50:10 -0700

    pulled in inquirer and added my main index.js

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+//Declare global variables and setup modules we need to pull in.
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+//Prompt array for asking user questions
+
+inquirer.prompt([{}]);
